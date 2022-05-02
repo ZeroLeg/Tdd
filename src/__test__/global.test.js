466394cf44a5daff321190c39f8f290bb83e4987
adrian <adrian@pinchaaqui.es>
2022-05-02 19:19:34 +0200

    pruebas de strings y array

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,6 +1,28 @@
 const text = "hola mundo";
-
+const fruits = ['manzana', 'melon', 'banana'];
 
 test('Debe contener un texto', () => {
     expect(text).toMatch(/mundo/);
+});
+
+test('Tenemos una banana', ()=> {
+    expect(fruits).toContain('banana');
+});
+
+test('Mayor que', ()=> {
+    expect(9).toBeGreaterThan(2);
+});
+
+test('Es verdadadero', () => {
+    expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+};
+
+test('probar callback', () => {
+    reverseString('Hola', (str) => {
+        expect(str).toBe('aloH');
+    })
 });
