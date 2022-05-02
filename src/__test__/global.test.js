792d04de6dd3c8a0b43bdf62e11f8a6b94f8312b
adrian <adrian@pinchaaqui.es>
2022-05-02 19:33:02 +0200

    pruebas basicas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,35 @@ test('probar callback', () => {
         expect(str).toBe('aloH');
     })
 });
+
+const reverseString2 = str => {
+    return new Promise( (resolve, reject )=> {
+        if(!str)
+            reject(Error("Error"))
+
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('provar promesa', () => {
+    return reverseString2('Hola')
+                .then(string => {
+                    expect(string).toBe('aloH')
+                })
+});
+
+test('test async / await', async () => {
+    const string = await reverseString2('hola');
+    expect(string).toBe('aloh');
+});
+
+afterEach(() => {
+    console.log("DEspues de cada prueba");
+});
+beforeEach(() => {
+    console.log('Antes de cada prueba');
+});
+
+beforeAll(() => {
+    console.log("Antes de todas las pruebas");
+});
